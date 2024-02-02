fef007cd01ca9c3173c5717235d722bbcec20155
uribo <ur1b0.dev@gmail.com>
2024-02-03 03:24:54 +0900

    指定した市区町村の現在までのデータを取得する

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 #!/usr/bin/env python3
 
+import requests
+from datetime import date
+
+
+def get_pollen_level_for_district(district,date):
+    # APIについて https://wxtech.weathernews.com/pollen/index.html
+    url = f"https://wxtech.weathernews.com/opendata/v1/pollen?citycode={district}&start={date}&end={date}"
+    r = requests.get(url)
+    csvList = r.text[:-1].split("\n")[1:]
+    return csvList
+
+
+citycode = 13103
+today = date.today().strftime("%Y%m%d")
+ret = get_pollen_level_for_district(district=citycode,date=today)
+print(f"{ret=}")
+
 
