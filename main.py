606f26db195d633df63f30a9cade851256d7e7bb
uribo <ur1b0.dev@gmail.com>
2024-02-03 04:04:09 +0900

    fix

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ def get_pollen_level_for_district(district,date):
 
     # 市区町村コード,日時,花粉飛散量/h のリストに変換
     # ex) pollenList[0]=['13103', '2024-02-03T00:00:00+09:00', '1']
-    hourlyPollenList = [i.split(",") for i in r.text[:-1].split("\n")[1:5]]
-    print(f"{hourlyPollenList[:5]=}")
+    hourlyPollenList = [i.split(",") for i in r.text[:-1].split("\n")[1:]]
+    print(f"{hourlyPollenList=}")
 
     # 花粉飛散量が0以上の数値の合計
     total_pollen_level = sum([int(i[2]) if int(i[2]) >= 0 else 0 for i in hourlyPollenList])
