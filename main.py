a9e062f2e5176a01aef34d3e42756557d86de7c7
uribo <ur1b0.dev@gmail.com>
2024-02-03 03:56:28 +0900

    正常値の合計を返すようにした

--- main.py
+++ main.py
@@ -8,13 +8,20 @@ def get_pollen_level_for_district(district,date):
     # APIについて https://wxtech.weathernews.com/pollen/index.html
     url = f"https://wxtech.weathernews.com/opendata/v1/pollen?citycode={district}&start={date}&end={date}"
     r = requests.get(url)
-    csvList = r.text[:-1].split("\n")[1:]
-    return csvList
+
+    # 市区町村コード,日時,花粉飛散量/h のリストに変換
+    # ex) pollenList[0]=['13103', '2024-02-03T00:00:00+09:00', '1']
+    hourlyPollenList = [i.split(",") for i in r.text[:-1].split("\n")[1:5]]
+    print(f"{hourlyPollenList[:5]=}")
+
+    # 花粉飛散量が0以上の数値の合計
+    total_pollen_level = sum([int(i[2]) if int(i[2]) >= 0 else 0 for i in hourlyPollenList])
+
+    return total_pollen_level
 
 
 citycode = 13103
 today = date.today().strftime("%Y%m%d")
 ret = get_pollen_level_for_district(district=citycode,date=today)
-print(f"{ret=}")
-
+print(f"{citycode=},{ret=}")
 
