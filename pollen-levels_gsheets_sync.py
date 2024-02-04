11b939c7ed0d2e08f4ad06d9aace4c92ec2a9936
uribo <ur1b0.dev@gmail.com>
2024-02-04 15:47:19 +0900

    google spread sheetsに書き込むようにした

--- pollen-levels_gsheets_sync.py
+++ pollen-levels_gsheets_sync.py
@@ -4,13 +4,50 @@ import requests
 from datetime import date
 import json
 import csv
+from oauth2client.service_account import ServiceAccountCredentials
+import gspread
 
 def main():
+
+    # 各地区の花粉飛散量を取得しリストにする
+    district_pollen_list = []
     for target in targets_list:
         citycode = target[1]
         today = date.today().strftime("%Y%m%d")
         ret = get_pollen_level_for_district(district=citycode,date=today)
+        district_pollen_list.append([target[0],ret])
         print(f"{target[0]},{citycode=},{ret=}")
+
+    # spread sheetを更新する範囲を作成 ex) "A1:B5"
+    data_header = [["地区","花粉飛散量"]]
+    data_range = "A1:"+chr(ord('A')+len(data_header[0])-1)+str(len(data_header + district_pollen_list))
+    print(f"{data_range=}")
+
+    # google spread sheets用に成形する
+    data_list = data_header + district_pollen_list
+    print(f"{data_list=}")
+    # 一次元に
+    data = []
+    for d in data_list:
+        data.extend(i for i in d)
+    print(f"{data=}")
+
+    # spread sheetを更新
+    ws = connect_gspread(gcp_key,gsheets_key)
+    ds= ws.range(data_range)
+    for i in range(len(data)):
+        ds[i].value = data[i]
+    ws.update_cells(ds)
+
+
+def connect_gspread(jsonf,key):
+#    scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+    scope = ['https://spreadsheets.google.com/feeds']
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(jsonf, scope)
+    gs = gspread.authorize(credentials)
+    worksheet = gs.open_by_key(key).sheet1
+    return worksheet
+
 
 def get_pollen_level_for_district(district,date):
     # APIについて https://wxtech.weathernews.com/pollen/index.html
@@ -34,6 +71,10 @@ with open("./config.json", "r") as f:
 with open(conf["targets"], "r") as f:
     targets_header = next(csv.reader(f))
     targets_list = [i for i in csv.reader(f)]
+
+# GCP サービスアカウントの秘密鍵と対象スプレッドシートの鍵の読み込み
+gcp_key = conf["gcp_key"]
+gsheets_key = conf["gspread_sheets_key"]
 
 if __name__ == '__main__':
     main()
